c470b49eebb6d55c2a1de2938bfc92b7b3d43fe4
Jeremy Roush <j@zinod.com>
2025-09-21 12:05:14 -0500

    Add auto-deploy workflow and version display
    - Added GitHub Actions deploy workflow triggered on release
    - Added version badge in bottom-right corner of UI
    - Configured GitHub repository secrets for SSH deployment
    - Fetches latest release tag from GitHub API and displays it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def github_release_version
+    Rails.cache.fetch('github_release_version', expires_in: 1.minute) do
+      begin
+        require 'net/http'
+        require 'json'
+
+        uri = URI('https://api.github.com/repos/jeremedia/shock-collar-portraits-2025/releases/latest')
+        response = Net::HTTP.get_response(uri)
+
+        if response.code == '200'
+          data = JSON.parse(response.body)
+          data['tag_name'] || 'dev'
+        else
+          'dev'
+        end
+      rescue => e
+        Rails.logger.error "Failed to fetch GitHub release version: #{e.message}"
+        'dev'
+      end
+    end
+  end
 end
